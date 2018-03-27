bf00b9c7a684da8286da837322cb0c5acb4ea087
Charlie Robinson <chr40@aber.ac.uk>
2018-03-27 14:56:48 +0100

    change to use startSquare and activeSquare

--- Assignment/functions.h
+++ Assignment/functions.h
@@ -1,19 +1,19 @@
 typedef struct box {
-	int ahead;
-	int back; 
+	int front;
+	int back;
 	int left;
 	int right;
-	
+
 	int isLowLight;
 	int currentSquare;
 	int wall;
-	
+
 	struct box *frontSquare;
-	struct box *behindSquare;
+	struct box *backSquare;
 	struct box *leftSquare;
 	struct box *rightSquare;
-}square;
+} square;
 
-square createBox(int l, int r, int b, int f);
+square createSquare(int l, int r, int b, int f);
 
 void movement();
--- Assignment/program.c
+++ Assignment/program.c
@@ -3,54 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+square activeSquare;
+// square startSquare;
+
 int main() {
 	FA_RobotInit();
 	FA_SetDriveSpeed(50);
 	FA_LCDBacklight(50);
 
-	square activeSquare = createBox(6,6,6,6);
+	activeSquare = createSquare(3, 3, 3, 3);
+	// activesquare = &startSquare;
 
-	while (FA_BTConnected() == 0){
-		FA_LCDPrint("BT Not Connected", 16, 20,25,FONT_NORMAL, LCD_OPAQUE);
+
+	while (FA_BTConnected() == 0) {
+		FA_LCDPrint("BT Not Connected", 16, 20, 25, FONT_NORMAL, LCD_OPAQUE);
 		FA_DelaySecs(2);
 		FA_LCDClear();
 	}
 
 	FA_LCDClear();
 
-	FA_BTSendUnsigned(activeSquare.ahead);
-	FA_BTSendString("\n", 15);
+	if (activeSquare.front != 10) {
+		FA_BTSendUnsigned(activeSquare.front);
+		FA_BTSendString("\n\n", 15);
+	}
 
-	
+
 
 	while (1) {
 
 
 		//check all of the walls to see which ones you can move to
-		unsigned short front = FA_ReadIR (2);
-		FA_BTSendString("front is\n", 9);
-		FA_BTSendUnsigned(front);
+		unsigned short frontIR = FA_ReadIR (2);
+		FA_BTSendString("frontIR is \n", 15);
+		FA_BTSendUnsigned(frontIR);
 		FA_BTSendString("\n", 2);
 
-		unsigned short rear = FA_ReadIR (6);
-		FA_BTSendString("rear is\n", 9);
-		FA_BTSendUnsigned(rear);
+		unsigned short rearIR = FA_ReadIR (6);
+		FA_BTSendString("rearIR is \n", 15);
+		FA_BTSendUnsigned(rearIR);
 		FA_BTSendString("\n", 2);
 
-		unsigned short left = FA_ReadIR (0);
-		FA_BTSendString("left is\n", 9);
-		FA_BTSendUnsigned(left);
+		unsigned short leftIR = FA_ReadIR (0);
+		FA_BTSendString("leftIR is \n", 15);
+		FA_BTSendUnsigned(leftIR);
 		FA_BTSendString("\n", 2);
 
-		unsigned short right = FA_ReadIR (4);
-		FA_BTSendString("right is\n", 9);
-		FA_BTSendUnsigned(right);
+		unsigned short rightIR = FA_ReadIR (4);
+		FA_BTSendString("rightIR is \n", 15);
+		FA_BTSendUnsigned(rightIR);
 		FA_BTSendString("\n", 2);
 
 
 		//priority of 0 is wall, 2 is new place to go and 1 is a place that you have been
-		
-		if (left>=170) {
+
+		if (leftIR >= 170) {
 			//left is blocked set it's priority to 0
 			activeSquare.left = 0;
 
@@ -61,7 +69,7 @@ int main() {
 
 			FA_LEDOn(0);
 		} else {
-			//this direction is unblocked so set it's prority to be 2 to reflect that 
+			//this direction is unblocked so set it's prority to be 2 to reflect that
 			activeSquare.left = 2;
 
 			FA_BTSendString("Left is now ", 15);
@@ -70,27 +78,27 @@ int main() {
 
 			FA_LEDOff(0);
 		}
-		
-		if (front>=313) {	
-			activeSquare.ahead = 0;
+
+		if (frontIR >= 313) {
+			activeSquare.front = 0;
 
 			FA_BTSendString("Front Blocked\n", 15);
 			FA_BTSendString("Front is now ", 15);
-			FA_BTSendUnsigned(activeSquare.ahead);
+			FA_BTSendUnsigned(activeSquare.front);
 			FA_BTSendString("\n", 15);
 
 			FA_LEDOn(2);
 		} else {
-			activeSquare.ahead = 2;
+			activeSquare.front = 2;
 
 			FA_BTSendString("Front is now ", 15);
-			FA_BTSendUnsigned(activeSquare.ahead);
+			FA_BTSendUnsigned(activeSquare.front);
 			FA_BTSendString("\n", 15);
 
 			FA_LEDOff(2);
 		}
-		
-		if (right>=242) {	
+
+		if (rightIR >= 242) {
 			activeSquare.right = 0;
 
 			FA_BTSendString("Right Blocked\n", 15);
@@ -108,8 +116,8 @@ int main() {
 
 			FA_LEDOff(4);
 		}
-		
-		if (rear>=595) {	
+
+		if (rearIR >= 595) {
 			activeSquare.back = 0;
 
 			FA_BTSendString("Back Blocked\n", 15);
@@ -133,57 +141,121 @@ int main() {
 
 		//now need to decide which direction to go
 		movement(activeSquare);
+
+		//set activesquare to the moved to square
+
+		FA_DelaySecs(2);
+		FA_BTSendString("\n\n\n", 15);
 	}
 
 
 
-	return 0;	
+	return 0;
 }
 
-void movement(square activeSquare){
-	if (activeSquare.ahead == 2) {
-			//go forwards
-			FA_Forwards(160);
-			return;
-	} 
+void movement(square activeSquare) {
+	square moveTo = createSquare(3, 3, 3, 3);
 
-	if (activeSquare.right == 2){
-			FA_Right(90);
-			FA_Forwards(160);
-			return;
-	} 
+	if (activeSquare.front == 2) {
+		FA_BTSendString("Going forwards\n", 15);
+		activeSquare.front = 1;
+		activeSquare.frontSquare = &moveTo;
 
-	if (activeSquare.left == 2){
-			FA_Left(90);
-			FA_Forwards(160);
-			return;
-	}  
+		FA_Forwards(150);
+		return;
+	}
+
+	if (activeSquare.right == 2) {
+		FA_BTSendString("Going right\n", 15);
+		activeSquare.right = 1;
+		activeSquare.rightSquare = &moveTo;
+
+		FA_Right(90);
+		FA_Forwards(150);
+		return;
+	}
+
+	if (activeSquare.left == 2) {
+		FA_BTSendString("Going left\n", 15);
+		activeSquare.left = 1;
+		activeSquare.leftSquare = &moveTo;
+
+		FA_Left(90);
+		FA_Forwards(150);
+		return;
+	}
 
 	if (activeSquare.back == 2) {
-			FA_Right(180);
-			FA_Forwards(160);
-			return;
-	} 
+		FA_BTSendString("Going back\n", 15);
+		activeSquare.back = 1;
+		activeSquare.backSquare = &moveTo;
 
-	while (1){
-		FA_LCDPrint("Error", 5, 20,25,FONT_NORMAL, LCD_OPAQUE);
+		FA_Right(180);
+		FA_Forwards(150);
+		return;
 	}
-	
+
+	if (activeSquare.front == 1) {
+		A_BTSendString("Going forwards\n", 15);
+		activeSquare.frontSquare = &moveTo;
+
+		FA_Forwards(150);
+		return;
+	}
+
+	if (activeSquare.right == 1) {
+		FA_BTSendString("Going right\n", 15);
+		activeSquare.rightSquare = &moveTo;
+
+		FA_Right(90);
+		FA_Forwards(150);
+		return;
+	}
+
+	if (activeSquare.left == 1) {
+		FA_BTSendString("Going left\n", 15);
+		activeSquare.leftSquare = &moveTo;
+
+		FA_Left(90);
+		FA_Forwards(150);
+		return;
+	}
+
+	if (activeSquare.back == 1) {
+		FA_BTSendString("Going back\n", 15);
+		activeSquare.backSquare = &moveTo;
+
+		FA_Right(180);
+		FA_Forwards(150);
+		return;
+	}
+
+
+	while (1) {
+		FA_LCDPrint("Error", 5, 20, 25, FONT_NORMAL, LCD_OPAQUE);
+		FA_BTSendString("No where to move to\n", 15);
+		FA_DelaySecs(1000);
+	}
 }
 
-square createBox(int l, int r, int b, int f) {
-	
-	square newNode = malloc(sizeof(square));
-	
+
+
+square createSquare(int l, int r, int b, int f) {
+	//wall is 0
+	//need to go through is 3
+	//gone through is 1
+
+	square newNode;
+
 	newNode.right = r;
 	newNode.left = l;
 	newNode.back = b;
-	newNode.ahead = f;
-	
-	newNode.frontSquare =NULL;
-	newNode.behindSquare =NULL;
-	newNode.leftSquare =NULL;
-	newNode.rightSquare =NULL;
-	
+	newNode.front = f;
+
+	newNode.frontSquare = NULL;
+	newNode.backSquare = NULL;
+	newNode.leftSquare = NULL;
+	newNode.rightSquare = NULL;
+
 	return newNode;
 }
